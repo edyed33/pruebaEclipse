a18ca219e305e92dfdd82a4e0b6325306b3ca8a9
laualpe <laualpe@hotmail.com>
2012-10-28 13:35:05 +0100

    union rama Laura

---
+++ src/pruebaLaura.java
@@ -0,0 +1,13 @@
+
+public class pruebaLaura {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es una prueba");
+
+	}
+
+}
